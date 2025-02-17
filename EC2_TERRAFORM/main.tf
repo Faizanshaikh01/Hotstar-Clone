2414908afad9290d71726584c513fcf5dbb6798f
Faizanshaikh01 <git@github.com:Faizanshaikh01/devops-repo.git>
2025-02-17 21:56:43 +0530

    deployment

---
+++ EC2_TERRAFORM/main.tf
@@ -0,0 +1,74 @@
+resource "aws_instance" "example" {
+  ami           = "ami-00bb6a80f01f03502"  # Use the latest Amazon Linux 2 AMI ID in your region
+  instance_type = "t2.micro"
+
+ associate_public_ip_address = true
+ vpc_security_group_ids     = [aws_security_group.jenkins_security_group_1.id]
+
+ key_name = "Docker"
+
+user_data = <<-EOF
+              #!/bin/bash
+              # Install docker
+              apt-get update
+              apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+              add-apt-repository \
+                 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+                 $(lsb_release -cs) \
+                 stable"
+              apt-get update
+              apt-get install -y docker-ce
+              usermod -aG docker ubuntu
+              EOF
+
+ tags = {
+    Name = "TerraformHotstar"
+  }
+}
+
+
+
+resource "aws_security_group" "jenkins_security_group_1" {
+  name        = "jenkins_security_group_1"
+  description = "Allows Port SSH and HTTP Traffic"
+
+  ingress {
+    description = "Allow SSH Traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS Traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow 8080 Traffic"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow 8080 Traffic"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
